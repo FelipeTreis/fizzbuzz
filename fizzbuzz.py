72d85e41542677337aad700c0dc9ff2145367901
Felipe Treis <felipetreis1@gmail.com>
2022-04-04 10:31:37 -0300

    Import project

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+"""
+Rules of Fizzbuzz
+
+1. If the position is multiple of 3: fizz
+2. If the position is multiple of 5: buzz
+3. If the position is multiple of 3 and 5: fizzbuzz
+4. For any other position speak the own number.
+"""
+
+
